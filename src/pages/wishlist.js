d37d673a2c77eee45e55c4a4c08887dfd640a4c7
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2024-02-04 22:58:50 -0500

    Try another script

--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -9,17 +9,19 @@ export default function Home() {
     <div>
       <Layout>
         <Helmet>
+        <title>Event Wishlist | Rebel Tents</title>
+        <meta name='description' content='Build a wishlist for your next event.' />
         <script type="module" src="https://unpkg.com/@goodshuffle/gspro-wc@0.5.6/dist/gspro-wc/gspro-wc.esm.js"></script>
         <script nomodule="" src="https://unpkg.com/@goodshuffle/gspro-wc@0.5.6/dist/gspro-wc/gspro-wc.js"></script>
         <script>
-            {`
-              document.addEventListener("DOMContentLoaded", function() {
-                const gsproWishlistConfig = document.createElement("gspro-wishlist-config");
-                gsproWishlistConfig.setAttribute("data-url", "https://data.goodshuffle.com/vendor/ZSLncVtdvLKr4REqp6mmM7WmiII6JT");
-                document.head.appendChild(gsproWishlistConfig);
-              });
-            `}
-          </script>
+          {`
+            window.addEventListener("load", function() {
+              const gsproWishlistConfig = document.createElement("gspro-wishlist-config");
+              gsproWishlistConfig.setAttribute("data-url", "https://data.goodshuffle.com/vendor/ZSLncVtdvLKr4REqp6mmM7WmiII6JT");
+              document.head.appendChild(gsproWishlistConfig);
+            });
+          `}
+        </script>
         </Helmet>
         <Topper headerText="Event Wishlist" />
         <div className="container mb-8 pr-8 pl-8">
