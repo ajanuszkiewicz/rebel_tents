914308b3c0fdc403099641d5a344831f0484cebd
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2024-02-04 22:19:34 -0500

    Try new integration method

--- src/pages/test.js
+++ src/pages/test.js
@@ -9,14 +9,21 @@ export default function Home() {
       <Layout>
         <Helmet>
         <script type="module" src="https://unpkg.com/@goodshuffle/gspro-wc@0.5.6/dist/gspro-wc/gspro-wc.esm.js"></script>
-<script nomodule="" src="https://unpkg.com/@goodshuffle/gspro-wc@0.5.6/dist/gspro-wc/gspro-wc.js"></script>
-<gspro-wishlist-config
-    data-url="https://data.goodshuffle.com/vendor/bMPD85AIWafd9CyAcSJ9gmsqYrF7Mi"
-></gspro-wishlist-config>
+        <script nomodule="" src="https://unpkg.com/@goodshuffle/gspro-wc@0.5.6/dist/gspro-wc/gspro-wc.js"></script>
+        <script>
+            {`
+              document.addEventListener("DOMContentLoaded", function() {
+                const gsproWishlistConfig = document.createElement("gspro-wishlist-config");
+                gsproWishlistConfig.setAttribute("data-url", "https://data.goodshuffle.com/vendor/ZSLncVtdvLKr4REqp6mmM7WmiII6JT");
+                document.head.appendChild(gsproWishlistConfig);
+              });
+            `}
+          </script>
         </Helmet>
         <Topper headerText="Testing Page" />
         <div className="container mb-8 pr-8 pl-8">
-<gspro-item-gallery></gspro-item-gallery>
+
+        <gspro-item-gallery></gspro-item-gallery>
         </div>
       </Layout>
     </div>
